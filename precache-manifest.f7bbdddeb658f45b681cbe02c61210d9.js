8a9ce4019f4b3a5c99a3c3782c52445bf78371c0
Filipe Bergami Alves <Filipeba.ict@gmail.com>
2020-05-17 10:40:49 +0100

    Updates

---
+++ precache-manifest.f7bbdddeb658f45b681cbe02c61210d9.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3459e15b80c9dc8924df8d1b5172b90",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5325b7b9282355c03726",
+    "url": "/portfolio/static/css/2.abd97b8c.chunk.css"
+  },
+  {
+    "revision": "e9fb75397477cda163bc",
+    "url": "/portfolio/static/css/main.fda53aba.chunk.css"
+  },
+  {
+    "revision": "5325b7b9282355c03726",
+    "url": "/portfolio/static/js/2.cbd68a0f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio/static/js/2.cbd68a0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9fb75397477cda163bc",
+    "url": "/portfolio/static/js/main.1bcb6baf.chunk.js"
+  },
+  {
+    "revision": "e5284505c87d0b24b753",
+    "url": "/portfolio/static/js/runtime-main.f4f2abbf.js"
+  },
+  {
+    "revision": "810a8951f369d5d04648e2504e7bc763",
+    "url": "/portfolio/static/media/facialrecognitionappfrontend.810a8951.PNG"
+  },
+  {
+    "revision": "ed8e25c816f9ce5dbd011dd305fe426e",
+    "url": "/portfolio/static/media/folder.ed8e25c8.svg"
+  },
+  {
+    "revision": "4142f8e7e035861eb4a92b360cf9a42e",
+    "url": "/portfolio/static/media/github.4142f8e7.svg"
+  },
+  {
+    "revision": "605cee18256761632dcff04f07a8cacc",
+    "url": "/portfolio/static/media/linkedin.605cee18.svg"
+  },
+  {
+    "revision": "7c5b9f6e30a708fd15354bfd0886054c",
+    "url": "/portfolio/static/media/location.7c5b9f6e.svg"
+  },
+  {
+    "revision": "70852fc806fcc8c6b78af59dfec1d312",
+    "url": "/portfolio/static/media/mail.70852fc8.svg"
+  },
+  {
+    "revision": "76a939e58278edff44025be1d26b5a79",
+    "url": "/portfolio/static/media/pdf.76a939e5.svg"
+  },
+  {
+    "revision": "aa352ce51892d948917335426180e449",
+    "url": "/portfolio/static/media/portfoliowebsitefrontpage.aa352ce5.PNG"
+  },
+  {
+    "revision": "18e167817c3460fed1558d1c8ed11761",
+    "url": "/portfolio/static/media/project.18e16781.svg"
+  },
+  {
+    "revision": "23428af953d760b807d4872c0fb82eca",
+    "url": "/portfolio/static/media/resume.23428af9.pdf"
+  },
+  {
+    "revision": "604c22d11ca7171bd07caf09477539fd",
+    "url": "/portfolio/static/media/trophy.604c22d1.svg"
+  },
+  {
+    "revision": "e48c08dd4153f01993c3263d93fbdc6e",
+    "url": "/portfolio/static/media/underdeveloptment.e48c08dd.jpg"
+  },
+  {
+    "revision": "951e91cef9ab9d51693a950117ac73a8",
+    "url": "/portfolio/static/media/weatherappfrontpage.951e91ce.png"
+  },
+  {
+    "revision": "902da8e25df6354a2581b8ae851c94f3",
+    "url": "/portfolio/static/media/web.902da8e2.svg"
+  }
+]);
